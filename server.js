fb7d3a51ccb84731a6cfcfb05189e1629dd1dd91
Paul <paul.killer@gmail.com>
2021-02-18 11:35:47 +1100

    fix deleting old sessions

--- server.js
+++ server.js
@@ -17,15 +17,25 @@ wss = new WebSocketServer({
 
 let squads = {};
 
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+};
+
 wss.on('request', function(request) {
     var ws = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' ws accepted.');
+    ws.id = wss.getUniqueID();
     ws.on('message', function(message) {
         try {
             const json = JSON.parse(message.utf8Data);
             if(json.message_type === 'join') {
+                ws.squad = json.data.squad;
                 if(!squads[json.data.squad]) squads[json.data.squad] = {};
-                if(!squads[json.data.squad][ws]) squads[json.data.squad][ws] = ws;
+                if(!squads[json.data.squad][ws.id]) squads[json.data.squad][ws.id] = ws;
+                console.log('ID [' + ws.id + '] joined [' + ws.squad + ']');
             } else {
                 Object.entries(squads[json.squad]).forEach(([, ws]) => ws.send(message.utf8Data));
             }
@@ -36,10 +46,10 @@ wss.on('request', function(request) {
         }
     });
     ws.on('close', function(reasonCode, description) {
-        for (var key in squads) {
-            if(Object.keys(squads[key]).length === 1) {
-                delete squads[key];
-            }
+        if(Object.keys(squads[ws.squad]).length === 1) {
+            delete squads[ws.squad];
+        } else {
+            delete squads[ws.squad][ws.id];
         }
         console.log((new Date()) + ' Peer ' + ws.remoteAddress + ' disconnected.');
     });
