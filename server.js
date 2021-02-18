0f8f5e62f58e3385a1366ba65bf96eb5995b0d86
Paul <paul.killer@gmail.com>
2021-02-18 11:00:25 +1100

    Migrate to use node-js server, test it out

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(80, function() {
-    console.log((new Date()) + ' Server is listening on port 80');
+server.listen(8001, function() {
+    console.log((new Date()) + ' Server is listening on port 8001');
 });
 
 wss = new WebSocketServer({
